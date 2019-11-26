6f0bd76fc8b117922564bf0dac7f53cc5ff17ccd
Nicholas Jackson <nicdogg360@gmail.com>
2019-11-26 11:08:03 -0800

    Version 1.55 added 'enter' button functionality

--- js/app.js
+++ js/app.js
@@ -1,38 +1,44 @@
 /* Program: TodoList web app
      Programmer: Nicholas Allen Jackson 
      Date: 11/15/2019 
-     Version: 1.35
+     Version: 1.55
      Purpose: Making a simple Todo List app, just flexing a few muscles */
 
- 
 //function definitions
-let add = () => {
-  //placed the user input string into variable
-  newTodo = todo_input.value;   
+let add = e => {
+  if (e.keyCode === 13 || e.target == add_btn) {
+    if (todo_input.value == "") 
+      alert("please enter a todo first! ");
+    else {
+      //placed the user input string into variable
+      newTodo = todo_input.value;
 
-  //created new li element with todo inside + self-destruct button
-  list_item = document.createElement('li');  
-  list_item.innerHTML = newTodo + '<button class="del_btn"> - </button>'  
-  
-  //attach todo to list body
-  todo_list.appendChild(list_item); 
- 
-  //reset user input 
-  todo_input.value = "";  
-} 
-let del = (e) => {
-  if(e.target && e.target.className == "del_btn"){ 
+      //created new li element with todo inside + self-destruct button
+      list_item = document.createElement("li");
+      list_item.innerHTML = newTodo + '<i class="fas fa-trash"></i>';
+
+      //attach todo to list body
+      todo_list.appendChild(list_item);
+
+      //reset user input
+      todo_input.value = "";
+    }
+  }
+};
+let del = e => {
+  //finds event using event delegation
+  if (e.target && e.target.className == "fas fa-trash") {
+    //removes the parent li element
     event.target.parentNode.remove();
   }
-}
+};
 
 //variable declarations
-let add_btn = document.getElementById('add_todo_btn'); 
-let todo_input = document.getElementById('todo_input');    
-let todo_list = document.getElementById('list'); 
-
+let add_btn = document.getElementById("add_todo_btn");
+let todo_input = document.getElementById("todo_input");
+let todo_list = document.getElementById("list");
 
 //event listener(s)
-add_btn.addEventListener('click',add); 
-todo_list.addEventListener('click',del);
-
+add_btn.addEventListener("click", add);
+todo_input.addEventListener("keydown", add);
+todo_list.addEventListener("click", del);
