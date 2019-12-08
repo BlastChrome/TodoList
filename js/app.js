c38bf5e32446e86b294aa5af76bc8fdc91fd2df7
Nicholas Jackson <nicdogg360@gmail.com>
2019-12-07 16:31:14 -0800

    Version with an edit feature, still needs tweeking

--- js/app.js
+++ js/app.js
@@ -4,30 +4,69 @@
      Version: 1.92
      Purpose: Making a simple Todo List app, just flexing a few muscles */
 
+
 //function definitions
-let add = e => { 
+let add = e => {
   //input validation
   if (e.keyCode === 13 || e.target == add_btn) {
-    if (todo_input.value == "") 
-      alert("please enter a todo first! ");
-    else { 
+    if (todo_input.value == "") alert("please enter a todo first! ");
+    else {
       //placed the user input string into variable
-      newTodo = todo_input.value; 
+      newTodo = todo_input.value;
 
       //created new li element with todo text,del-btn,and edit-btn;
-      list_item = document.createElement("li"); 
-      list_actions = document.createElement('div'); 
+      list_item = document.createElement("li");
+      list_actions = document.createElement("div");
       list_actions.className += "list_actions";
-      list_item.innerHTML = newTodo + '<div><i class="far fa-edit btn"> </i>'
-                                    + '<i class="fas fa-trash btn"></i> </div>';  
-      
-      
+      list_item.innerHTML =
+        "<p>" +
+        newTodo +
+        "</p>" +
+        '<div><i class="far fa-edit btn"> </i>' +
+        '<i class="fas fa-trash btn"></i> </div>';
+
       //attach todo to list body
-      todo_list.appendChild(list_item); 
-    
+      todo_list.appendChild(list_item);
+
       //reset user input text field
       todo_input.value = "";
     }
+  }
+};
+let clear = () => {
+  //checks to see if the list is already empty
+  if (todo_list.childElementCount < 1) alert("list empty!");
+  else {
+    do { 
+      answer = prompt(
+        "this action will delete the ENTIRE todo list, will you proceed?(Y/N)"
+      ); 
+      //final deletion confirmation 
+      if (answer === "y" || answer === "Y") {
+        todo_list.innerHTML = "";
+        alert("deletion complete! ");
+        break;
+      } else if (answer === "n" || answer === "N") {
+        alert("OK!");
+        break;
+      } else {
+        if (answer == "") alert("Nigga did i say you coud write nothing??");
+        else alert("Nigga did i say you coud write " + answer + "??");
+        alert("now do it again, dumbass!");
+      }
+    } while (answer != "Y" || answer != "y" || answer != "n" || answer != "N");
+  }
+};
+let edit = e => {
+  //checks for edit btn class
+  if (e.target && e.target.className == "far fa-edit btn") { 
+    // redo is set to the first li element(the todo <p></p>);
+    redo = e.target.parentNode.parentNode.firstElementChild;
+    do {
+      redo.textContent = prompt("Edit " + redo.textContent);
+      if (redo.textContent.length > 27 || redo.textContent.length < 1)
+        alert("LMAO THATS WAY TO LONG/SHORT YOU FUCK!");
+    } while (redo.textContent.length > 27);
   }
 };
 let del = e => {
@@ -37,44 +76,19 @@ let del = e => {
     event.target.parentNode.parentNode.remove();
   }
 };
-let clear = e =>{
-  if(todo_list.childElementCount < 1)
-    alert("list empty!");  
-  else{ 
-    do{
-      answer = prompt("this action will delete the ENTIRE todo list, will you proceed?(Y/N)"); 
-      if(answer === 'y' || answer === 'Y'){
-        todo_list.innerHTML = '';   
-        alert("deletion complete! "); 
-        break;
-      }
-      else if( answer === 'n' || answer === 'N'){
-        alert("OK!"); 
-        break;
-      } 
-      else {
-        if(answer == '') 
-          alert("Nigga did i say you coud write nothing??"); 
-        else
-          alert("Nigga did i say you coud write " + answer + "??"); 
-        alert('now do it again, dumbass!');
-      } 
-    } while(answer != 'Y' || answer != 'y' || answer != 'n' || answer != 'N');
-  }
-}; 
 
-let edit = e =>{
-
-};
-
-//variable declarations 
-let add_btn = document.getElementById("add_todo_btn"); 
+//variable declarations
+let add_btn = document.getElementById("add_todo_btn");
 let clear_btn = document.getElementById("clear_todo_btn");
 let todo_input = document.getElementById("todo_input");
 let todo_list = document.getElementById("list");
 
 //event listener(s)
-add_btn.addEventListener("click", add); 
+add_btn.addEventListener("click", add);
 todo_input.addEventListener("keydown", add);
-clear_btn.addEventListener('click',clear);
+clear_btn.addEventListener("click", clear); 
+
+//todo item(s) listener(s)
 todo_list.addEventListener("click", del);
+todo_list.addEventListener("click", edit); 
+
