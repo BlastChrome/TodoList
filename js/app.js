2a4d1b71efe653ddf42551b3671aebaaf74c4337
Nick <nicdogg360@gmail.com>
2020-07-05 12:02:26 -0700

    more style changes, need tro clean up CSS

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ let add = e => {
         "<p>" +
         newTodo +
         "</p>" +
-        '<div><i class="far fa-edit btn"> </i>' +
-        '<i class="fas fa-trash btn"></i> </div>';
+        '<div class="icon-container"><i class="far fa-edit btn"> </i></div>' +
+        '<div><i class="fas fa-trash btn"></i></div> ';
 
       //attach todo to list body
       todo_list.appendChild(list_item);
