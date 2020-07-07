4ba1e74ed61a10c012b4df02902ed33c0f1df1a2
Nick <nicdogg360@gmail.com>
2020-07-06 17:54:43 -0700

    some even listeners, need to find out jq stuff

--- js/app.js
+++ js/app.js
@@ -1,100 +1,22 @@
-/* Program: TodoList web app
-     Programmer: Nicholas Allen Jackson 
-     Date: 11/15/2019 
-     Version: 1.92
-     Purpose: Making a simple Todo List app, just flexing a few muscles */
+// Variable declarations
+$add_btn = $('#add-btn'); 
+$clr_btn = $('#delete-btn'); 
+$input = $('#todo-input'); 
+$list = $('ol');
 
-//function definitions
-let add = e => {
-  //input validation
-  if (e.keyCode === 13 || e.target == add_btn) {
-    if (todo_input.value == "") alert("please enter a todo first! ");
-    else {
-      //placed the user input string into variable
-      newTodo = todo_input.value;
-
-      //created new li element with todo text,del-btn,and edit-btn;
-      list_item = document.createElement("li");
-      list_actions = document.createElement("div");
-      list_actions.className += "list_actions";
-      list_item.innerHTML =
-        "<p>" +
-        newTodo +
-        "</p>" +
-        '<div class="icon-container"><i class="far fa-clipboard btn"> </i></div>' +
-        '<div><i class="fas fa-trash-alt btn"></i></div> ';
-
-      //attach todo to list body
-      todo_list.appendChild(list_item);
-
-      //reset user input text field
-      todo_input.value = "";
+// function declarations 
+let add = e =>{ 
+    if (e.which === 13 || e.target === $add_btn){
+        if($input.val() == '')
+            alert('please enter a todo first')
     }
-  }
 };
-let clear = () => {
-  //checks to see if the list is already empty
-  if (todo_list.childElementCount < 1) alert("list empty!");
-  else {
-    do {
-      answer = prompt(
-        "this action will delete the ENTIRE todo list, will you proceed?(Y/N)"
-      );
-      //final deletion confirmation
-      if (answer === "y" || answer === "Y") {
-        todo_list.innerHTML = "";
-        alert("deletion complete! ");
-        break;
-      } else if (answer === "n" || answer === "N" || answer == '') {
-        alert("OK!");
-        break;
-      } else {
-        if (answer == "") alert("Nigga did i say you coud write nothing??");
-        else alert("Nigga did i say you coud write " + answer + "??");
-        alert("now do it again, dumbass!");
-      }
-    } while (answer != "Y" || answer != "y" || answer != "n" || answer != "N");
-  }
-};
-let edit = e => {
-  //checks for edit btn class
-  if (e.target && e.target.className == "far fa-clipboard btn") {
-    // redo is set to the first li element(the todo <p></p>);
-    redo = e.target.parentNode.parentNode.firstElementChild;
-    redoN = redo.textContent;
-    do {
-      redo.textContent = prompt("Edit " + redo.textContent);
-      if (redo.textContent.length > 27 || redo.textContent.length < 1) {
-        if (redo.textContent.length > 27) {
-          alert("LMAO THATS WAY TO LONG YOU FUCK!");
-          redo.textContent = redoN;
-        } else if (redo.textContent.length < 1){
-          alert("LMAO THATS WAY TO SHORT YOU FUCK!");
-          redo.textContent = redoN;
-        }
-      }
-    } while (redo.textContent.length > 27);
-  }
-};
-let del = e => {
-  //finds event using event delegation
-  if (e.target && e.target.className == "fas fa-trash-alt btn") {
-    //removes the parent li element
-    event.target.parentNode.parentNode.remove();
-  }
-};
+let clr = e => {
+    console.log("clear btn clicked");
+}
 
-//variable declarations
-let add_btn = document.getElementById("add_todo_btn");
-let clear_btn = document.getElementById("clear_todo_btn");
-let todo_input = document.getElementById("todo_input");
-let todo_list = document.getElementById("list");
+// function call
+$add_btn.on('click', add); 
+$clr_btn.on('click', clr); 
+$input.on('keypress', add); 
 
-//event listener(s)
-add_btn.addEventListener("click", add);
-todo_input.addEventListener("keydown", add);
-clear_btn.addEventListener("click", clear);
-
-//todo item(s) listener(s)
-todo_list.addEventListener("click", del);
-todo_list.addEventListener("click", edit);
